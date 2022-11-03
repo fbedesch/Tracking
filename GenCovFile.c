0098b95e1716a55599e04eda51e5143b9f899dd0
Franco BEDESCHI <bed@fnal.gov>
2022-11-03 15:27:09 +0100

    Increased grid to 50 TeV

---
+++ GenCovFile.c
@@ -0,0 +1,23 @@
+﻿#include <TMath.h>
+#include <TGraph.h>
+#include <TAxis.h>
+#include <TLegend.h>
+#include <TCanvas.h>
+#include <TGraph.h>
+#include <TString.h>
+#include <iostream>
+#include "SolGeom.h"
+#include "SolGridCov.h"
+
+//
+void GenCovFile(char * GeoFile, TString CovFile)
+{
+	//
+	//	Init geometry
+	//
+	SolGeom *G = new SolGeom(GeoFile);
+	//
+	// Write grid
+	SolGridCov* GC = new SolGridCov();
+	GC->Write(CovFile, G);
+}
