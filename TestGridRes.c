b3c66f9220045477e33ac85c6c550d0882d5184c
Franco BEDESCHI <bed@fnal.gov>
2022-11-08 16:21:00 +0100

    Fix error for angles > 90 degrees

--- TestGridRes.c
+++ TestGridRes.c
@@ -1,4 +1,4 @@
-﻿
+﻿//
 #include <TMath.h>
 #include <TGraph.h>
 #include <TGraphErrors.h>
@@ -16,14 +16,14 @@
 #include "SolTrack.h"
 #include "SolGridCov.h"
 //
-void TestCompRes(Double_t Ang)
+void TestGridRes(Double_t Perp, Double_t Ang)
 {
 	//
 	// Track to draw
 	Double_t x[3] = { 0.0, 0.0, 0.0 };		// Track starting point
-	Double_t ppt = 10.;						// Track pt
+	Double_t ppt = 10.;				// Track pt
 	Double_t th = Ang * TMath::Pi() / 180.;
-	Double_t ppz = ppt / TMath::Tan(th);	// Track pz
+	Double_t ppz = ppt / TMath::Tan(th);		// Track pz
 	Double_t p[3] = { ppt, 0.0, ppz };		// Track momentum
 	//
 	//**************************
@@ -31,62 +31,38 @@ void TestCompRes(Double_t Ang)
 	//**************************
 	//
 	//
-	SolGeom *Gidea;			// Initialize IDEA geometry
-	Gidea = new SolGeom("GeoIDEA_BASE.txt");	// Geometry IDEA
-	Gidea->Draw();			// Draw IDEA geometry
-	TCanvas *cc_id = Gidea->cnv();					// Get canvas with geo display
-	cc_id->cd(1);
-	SolTrack *trk_id = new SolTrack(x, p, Gidea);	// Initialize track
-	TGraph *gr_id = trk_id->TrkPlot();			// graph intersection with layers
-	gr_id->Draw("PLSAME");						// plot track
-	TCanvas *cnv_id = new TCanvas("cnv_id", "IDEA Geometry");
-	cnv_id->Divide(1, 1); cnv_id->cd(1); cc_id->DrawClonePad();
+	SolGeom* Gid = new SolGeom("GeoIDEA_BASE.txt");
+	SolGridCov *Gidea = new SolGridCov();	// Initialize IDEA geometry
+	Gidea->Read("CovIDEA_BASE.root");	// Geometry IDEA
 	//
-	SolGeom *Gcld;			// Initialize CLD  geometry	
-	Gcld  = new SolGeom("GeoCLD.txt");			// Geometry CLD
-	Gcld->Draw();			// Draw CLD  geometry
-	TCanvas *cc_cl = Gcld->cnv();
-	cc_cl->cd(1);
-	SolTrack *trk_cl = new SolTrack(x, p, Gcld);	// Initialize track
-	TGraph *gr_cl = trk_cl->TrkPlot();			// graph intersection with layers
-	gr_cl->Draw("PLSAME");						// plot track
-	TCanvas *cnv_cl = new TCanvas("cnv_cl", "CLD Geometry");
-	cnv_cl->Divide(1, 1); cnv_cl->cd(1); cc_cl->DrawClonePad();
-	cc_cl->Close(); gSystem->ProcessEvents(); delete cc_cl; cc_cl = 0;
+	SolGridCov *Gcld = new SolGridCov();	// Initialize CLD  geometry	
+	Gcld->Read("CovCLD.root");		// Geometry CLD
 	//
-	//
-	// Geometry initialized and drawn
-	//********************************************************
-	//	
 	//******************************************************
-	// Compare track parameter resolutions vs pt and theta *
+	// Compare track parameter resolutions vs pt           *
 	//******************************************************
 	//
-	TCanvas *resol = new TCanvas("resol", "Comparison of resolutions", 100, 100, 500, 500);
+	TCanvas *resol = new TCanvas("resol", "Comparison of resolutions vs pt ", 100, 100, 500, 500);
 	resol->Divide(2, 2);
 	// Define graphs for IDEA
 	TGraph *grpt_id;				// pt resolution graphs
-	TGraph *grptms_id;				// pt resolution graphs MS only
 	TGraph *grd0_id;				// D resolution graphs
 	TGraph *grz0_id;				// z0 resolution graphs
 	TGraph *grth_id;				// theta resolution
 	// Define graphs for CLD
 	TGraph *grpt_cl;				// pt resolution graphs
-	TGraph *grptms_cl;				// pt resolution graphs MS only
 	TGraph *grd0_cl;				// D resolution graphs
 	TGraph *grz0_cl;				// z0 resolution graphs
 	TGraph *grth_cl;				// theta resolution
 	// Setup graph arrays
-	Int_t Npt = 200;			// Nr. of points per graph
+	Int_t Npt = 200;				// Nr. of points per graph
 	Double_t * pt = new Double_t[Npt];
 	Double_t * pp = new Double_t[Npt];
 	Double_t *spt_id = new Double_t[Npt];
-	Double_t *sptms_id = new Double_t[Npt];
 	Double_t *sd0_id = new Double_t[Npt];
 	Double_t *sz0_id = new Double_t[Npt];
 	Double_t *sth_id = new Double_t[Npt];
 	Double_t *spt_cl = new Double_t[Npt];
-	Double_t *sptms_cl = new Double_t[Npt];
 	Double_t *sd0_cl = new Double_t[Npt];
 	Double_t *sz0_cl = new Double_t[Npt];
 	Double_t *sth_cl = new Double_t[Npt];
@@ -98,36 +74,36 @@ void TestCompRes(Double_t Ang)
 	{
 		Double_t x[3]; Double_t p[3];
 		x[0] = 0; x[1] = 0; x[2] = 0;			// Set origin
-		pt[k] = ptmin+k*pts;					// Set transverse momentum
+		pt[k] = ptmin+k*pts;				// Set transverse momentum
 		p[0] = pt[k]; p[1] = 0;	
 		pp[k] = pt[k]/TMath::Sin(th);			// Set momentum
 		p[2] = TMath::Sqrt(pp[k] * pp[k] - p[0] * p[0]);
 		// Fill IDEA arrays
-		SolTrack *tr_id = new SolTrack(x, p, Gidea);	// Initialize track
-		Bool_t Res = kTRUE;		// Enable detector resolution effects
-		Bool_t MS  = kTRUE;		// Enable multiple scattering
-		tr_id->CovCalc(Res,MS);					// Calculate covariance
-		spt_id[k] = tr_id->s_pt();							// Dpt/pt
-		sd0_id[k] = tr_id->s_D()*1e6;							// D  res. - change to microns
-		sz0_id[k] = tr_id->s_z0()*1e6;						// z0 res. - change to microns
-		sth_id[k] = tr_id->s_ct() / (1 + pow(tr_id->ct(), 2));	// theta resolution
+		Double_t theta = TMath::ATan2(pt[k],p[2])*180./TMath::Pi();
+		TMatrixDSym cov_id = Gidea->GetCov(pt[k],theta);
+		Double_t sD_id  = TMath::Sqrt(cov_id(0,0));
+		Double_t sPh_id = TMath::Sqrt(cov_id(1,1));
+		Double_t sC_id  = TMath::Sqrt(cov_id(2,2));
+		Double_t sPt_id = 2 * sC_id*pt[k] / (0.2998*Gid->B());
+		Double_t sZ_id  = TMath::Sqrt(cov_id(3,3));
+		Double_t sCt_id = TMath::Sqrt(cov_id(4,4));
+		spt_id[k] = sPt_id;				// Dpt/pt
+		sd0_id[k] = sD_id*1e6;				// D  res. - change to microns
+		sz0_id[k] = sZ_id*1e6;				// z0 res. - change to microns
+		sth_id[k] = sCt_id / (1 + pow(p[2]/pt[k], 2));	// theta resolution
 		//
-		Res = kFALSE;
-		MS = kTRUE;
-		tr_id->CovCalc(Res,MS);					// Calculate covariance with only MS
-		sptms_id[k] = tr_id->s_pt();							// Dpt/pt
 		// Fill CLD arrays
-		SolTrack *tr_cl = new SolTrack(x, p, Gcld);	// Initialize track
-		Res = kTRUE;		// Enable detector resolution effects
-		MS  = kTRUE;		// Enable multiple scattering
-		tr_cl->CovCalc(Res,MS);					// Calculate covariance
-		spt_cl[k] = tr_cl->s_pt();							// Dpt/pt
-		sd0_cl[k] = tr_cl->s_D()*1e6;							// D  res. - change to microns
-		sz0_cl[k] = tr_cl->s_z0()*1e6;						// z0 res. - change to microns
-		sth_cl[k] = tr_cl->s_ct() / (1 + pow(tr_cl->ct(), 2));	// theta resolution
-		Res = kFALSE;
-		tr_cl->CovCalc(Res,MS);					// Calculate covariance with only MS
-		sptms_cl[k] = tr_cl->s_pt();							// Dpt/pt
+		TMatrixDSym cov_cl = Gcld->GetCov(pt[k],theta);
+		Double_t sD_cl  = TMath::Sqrt(cov_cl(0,0));
+		Double_t sPh_cl = TMath::Sqrt(cov_cl(1,1));
+		Double_t sC_cl  = TMath::Sqrt(cov_cl(2,2));
+		Double_t sPt_cl = 2 * sC_cl*pt[k] / (0.2998*Gid->B());
+		Double_t sZ_cl  = TMath::Sqrt(cov_cl(3,3));
+		Double_t sCt_cl = TMath::Sqrt(cov_cl(4,4));
+		spt_cl[k] = sPt_cl;				// Dpt/pt
+		sd0_cl[k] = sD_cl*1e6;				// D  res. - change to microns
+		sz0_cl[k] = sZ_cl*1e6;				// z0 res. - change to microns
+		sth_cl[k] = sCt_cl / (1 + pow(p[2]/pt[k], 2));	// theta resolution
 	}
 	//
 	// Compare pt resolution
@@ -139,14 +115,6 @@ void TestCompRes(Double_t Ang)
 	grpt_cl->SetMinimum(0.0);
 	grpt_cl->GetXaxis()->SetTitle("pt (GeV)");
 	grpt_cl->Draw("APL");
-	grptms_cl = new TGraph(Npt, pt, sptms_cl);			// pt resolution MS only
-	grptms_cl->SetLineColor(kRed);
-	grptms_cl->SetMarkerColor(kRed);
-	grptms_cl->SetLineStyle(7);
-	grptms_cl->SetTitle("#sigma_{pt}/pt");
-	grptms_cl->SetMinimum(0.0);
-	grptms_cl->GetXaxis()->SetTitle("pt (GeV)");
-	grptms_cl->Draw("SAME");
 	grpt_id = new TGraph(Npt, pt, spt_id);			// pt resolution
 	grpt_id->SetLineColor(kBlue);
 	grpt_id->SetMarkerColor(kBlue);
@@ -154,23 +122,13 @@ void TestCompRes(Double_t Ang)
 	grpt_id->SetMinimum(0.0);
 	grpt_id->GetXaxis()->SetTitle("pt (GeV)");
 	grpt_id->Draw("SAME");
-	grptms_id = new TGraph(Npt, pt, sptms_id);			// pt resolution MS only
-	grptms_id->SetLineColor(kBlue);
-	grptms_id->SetMarkerColor(kBlue);
-	grptms_id->SetLineStyle(7);
-	grptms_id->SetTitle("#sigma_{pt}/pt");
-	grptms_id->SetMinimum(0.0);
-	grptms_id->GetXaxis()->SetTitle("pt (GeV)");
-	grptms_id->Draw("SAME");
 	Int_t iang = TMath::Nint(Ang);
 	TLegend *lgpt = new TLegend(0.2, 0.9, 0.6, 0.70);
 	TString LgTitle; 
 	LgTitle.Form("Track angle %d deg.",iang);
 	lgpt->SetHeader(LgTitle);
 	lgpt->AddEntry(grpt_id, "IDEA", "L");
-	lgpt->AddEntry(grptms_id, "IDEA MS only", "L");
 	lgpt->AddEntry(grpt_cl, "CLD", "L");
-	lgpt->AddEntry(grptms_cl, "CLD MS only", "L");
 	lgpt->Draw();
 	// Compare d0 resolution
 	resol->cd(2);
